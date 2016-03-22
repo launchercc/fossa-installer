28baa7990d50999369783cc73eb63d0cd753eb42
Abraham Elmahrek <abe@tldrlegal.com>
2016-03-22 13:20:35 -0700

    Removing an existing container only when it exists

--- boot.sh
+++ boot.sh
@@ -4,13 +4,13 @@ source ./config.sh
 # Fetch latest image
 docker pull fossa/fossa:latest
 
-# Kill running image
 if [ $( docker ps -aq ) ]; then
+	# Kill running image
 	docker kill $(docker ps -aq);
+	
+	# Remove existing container
+	docker rm $(docker ps -aq)
 fi;
-
-# Remove existing container
-docker rm $(docker ps -aq)
 
 # Remove image entirely
 docker rmi $(docker images -f "dangling=true" -q)
