8d758ec1d3fc768f3dea145667258d8615b2ceb3
Abraham Elmahrek <abe@tldrlegal.com>
2016-03-22 13:17:33 -0700

    Stop containers only when they exist

--- boot.sh
+++ boot.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 source ./config.sh
 
+# Fetch latest image
 docker pull fossa/fossa:latest
-docker kill $(docker ps -aq)
+
+# Kill running image
+if [ $( docker ps -aq ) ]; then
+	docker kill $(docker ps -aq);
+fi;
+
+# Remove existing container
 docker rm $(docker ps -aq)
+
+# Remove image entirely
 docker rmi $(docker images -f "dangling=true" -q)
 
 # run agents
