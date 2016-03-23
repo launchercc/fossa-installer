16b856cac6484c099454cbe6530e33e0a44dfd12
Abraham Elmahrek <abe@tldrlegal.com>
2016-03-23 00:05:26 -0700

    change cli output

--- boot.sh
+++ boot.sh
@@ -39,19 +39,17 @@ function init {
 }
 
 function start {
-  printf "Starting Fossa"
+  printf "Starting Fossa!"
   NUMBER_OF_AGENTS=${1-4}
 
   # run agents
   while [ ${NUMBER_OF_AGENTS} -gt 0 ]; do
-    printf "."
     docker run --env-file ${TOP_DIR}/config.env fossa/fossa:latest npm run start:agent 2>&1 > /dev/null &
     (( NUMBER_OF_AGENTS-- ))
   done;
 
   # run core server
   docker run --env-file ${TOP_DIR}/config.env -p 80:80 -p 443:443 fossa/fossa:latest npm run start 2>&1 > /dev/null &
-  echo ".done!"
 }
 
 function stop {
@@ -60,14 +58,10 @@ function stop {
   current=$( runninginstances )
 
   # Kill running image
-  printf "."
   docker kill ${current} > /dev/null
   
   # Remove existing container
-  printf "."
   docker rm -f ${current} > /dev/null
-
-  printf ".done!"
 }
 
 case "$1" in
