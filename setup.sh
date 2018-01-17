5a4d33b0440991a4099efd0e30688fc5fa65b887
xizhao <kevin@fossa.io>
2018-01-17 15:38:10 -0800

    Pull pg from automated setup

--- setup.sh
+++ setup.sh
@@ -36,12 +36,12 @@ function setup_system {
   apt-get -y install apt-transport-https ca-certificates
   apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-  # Download and install docker, postgres
+  # Download and install docker
   # NOTE: Do not use Docker 1.9.1 because of: https://github.com/docker/docker/issues/18180
   grep -Fq "deb https://apt.dockerproject.org/repo ubuntu-trusty main" < /etc/apt/sources.list.d/docker.list || ( touch /etc/apt/sources.list.d/docker.list ; echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list )
   apt-get update
   apt-get purge lxc-docker
-  apt-get install -y docker-engine postgresql-9.3 postgresql-contrib-9.3 postgresql-server-dev-9.3 curl tar default-jdk
+  apt-get install -y docker-engine curl tar default-jdk
 
   # Replace "ubuntu" with your username, if it's different
   usermod -aG docker ubuntu
